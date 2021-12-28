2ea6defa9c990db8ffb081ef08429cb3abc34c3c
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-28 13:21:41 -0300

    add devtools to proyect and traicing: true

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -6,6 +6,8 @@ const unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugAdded("Bug 2"));
+store.dispatch(bugAdded("Bug 3"));
 
 store.dispatch(bugResolved(1));
 
