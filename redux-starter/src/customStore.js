1cb5fca61d31cb15ff75fccc702754296152f9f1
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-28 12:40:00 -0300

    subscribing to the store

--- redux-starter/src/customStore.js
+++ redux-starter/src/customStore.js
@@ -2,15 +2,22 @@ import reducer from "./reducer";
 
 function createStore(reducer) {
   let state;
+  let listeners = [];
+
+  function subscribe(listener) {
+    listeners.push(listener);
+  }
 
   function dispatch(action) {
     state = reducer(state, action);
+    for (let i = 0; i < listeners.length; i++) listeners[i]();
   }
 
   function getState() {
     return state;
   }
   return {
+    subscribe,
     getState,
     dispatch,
   };
--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,6 +1,10 @@
 import store from "./customStore";
 import * as actions from "./actions";
 
+store.subscribe(() => {
+  console.log("Store change!");
+});
+
 store.dispatch(actions.bugAdded("Bug 1"));
 
 console.log(store.getState());
