3a1ff1608ef21566867a6335998dbb2d7838feb1
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-28 12:33:30 -0300

    dispatching actions

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,20 +1,6 @@
-// import store from "./store";
-// import { bugAdded, bugRemoved, bugResolved } from "./actions";
-
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store changed!", store.getState());
-// });
-
-// store.dispatch(bugAdded("Bug 1"));
-
-// store.dispatch(bugResolved(1));
-
-// unsubscribe(); //para cancelar subcripcion a la store
-
-// store.dispatch(bugRemoved(1));
-
-// console.log(store.getState());
-
 import store from "./customStore";
+import * as actions from "./actions";
+
+store.dispatch(actions.bugAdded("Bug 1"));
 
 console.log(store.getState());
