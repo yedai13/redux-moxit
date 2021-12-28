c2f331e85329b85953607a7fe6d292c3e448c2fa
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-28 11:26:54 -0300

    subscribing to the store

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,11 +1,17 @@
 import store from "./store";
 
+const unsubscribe = store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+});
+
 store.dispatch({
   type: "bugAdded",
   payload: {
     description: "bug1",
   },
 });
+
+unsubscribe(); //para cancelar subcripcion a la store
 
 store.dispatch({
   type: "bugRemoved",
