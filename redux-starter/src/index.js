822f9c0121de733f114bab4a6c6b65de4dd19a64
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-28 12:24:01 -0300

    private properties

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,16 +1,20 @@
-import store from "./store";
-import { bugAdded, bugRemoved, bugResolved } from "./actions";
+// import store from "./store";
+// import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
-const unsubscribe = store.subscribe(() => {
-  console.log("Store changed!", store.getState());
-});
+// const unsubscribe = store.subscribe(() => {
+//   console.log("Store changed!", store.getState());
+// });
 
-store.dispatch(bugAdded("Bug 1"));
+// store.dispatch(bugAdded("Bug 1"));
 
-store.dispatch(bugResolved(1));
+// store.dispatch(bugResolved(1));
 
-unsubscribe(); //para cancelar subcripcion a la store
+// unsubscribe(); //para cancelar subcripcion a la store
 
-store.dispatch(bugRemoved(1));
+// store.dispatch(bugRemoved(1));
+
+// console.log(store.getState());
+
+import store from "./customStore";
 
 console.log(store.getState());
