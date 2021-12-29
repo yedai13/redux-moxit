8aba20a716def2457253e3e97a75b3981e47686e
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-29 11:28:12 -0300

    creating the store - toolkit

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -1,8 +1,14 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
-export default function configureStore() {
-  const store = createStore(reducer, devToolsEnhancer({ trace: true }));
-  return store;
+export default function () {
+  return configureStore({ reducer });
 }
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "redux-devtools-extension";
+// import reducer from "./bugs";
+
+// export default function configureStore() {
+//   const store = createStore(reducer, devToolsEnhancer({ trace: true }));
+//   return store;
+// }
