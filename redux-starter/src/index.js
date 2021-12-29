6d466388da0abcff4b9f06d4be94d70d3c0f8fe7
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-29 12:59:41 -0300

    create projects reducer/actions

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,11 +1,14 @@
 import configureStore from "./store/configureStore";
 import { bugAdded, bugRemoved, bugResolved } from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore();
 
 const unsubscribe = store.subscribe(() => {
   console.log("Store changed!", store.getState());
 });
+
+store.dispatch(projectAdded({ name: "Project 1" }));
 
 store.dispatch(bugAdded({ description: "Bug 1" }));
 store.dispatch(bugAdded({ description: "Bug 2" }));
