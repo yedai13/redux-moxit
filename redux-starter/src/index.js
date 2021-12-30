cbfcc450ac09bca74ff56045fa6f0bdb15aba329
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-30 13:33:03 -0300

    getting data from the server

--- redux-starter/src/index.js
+++ redux-starter/src/index.js
@@ -1,65 +1,6 @@
 import configureStore from "./store/configureStore";
-import {
-  bugAdded,
-  bugRemoved,
-  bugResolved,
-  getUnresolvedBugs,
-  bugAssignedToUser,
-  getBugsByUser,
-} from "./store/bugs";
-import { projectAdded } from "./store/projects";
-import { userAdded } from "./store/users";
-import * as actions from "./store/api";
+import { loadBugs } from "./store/bugs";
 
 const store = configureStore();
 
-store.dispatch(
-  actions.apiCallBegan({
-    url: "/bugs",
-    onSuccess: "bugsReceived",
-  })
-);
-
-// store.dispatch((dispatch, getState) => {
-//   //call an API
-//   //when the promise is resolved => dispatch()
-//   dispatch({ type: "bugsReceived", bugs: [1, 2, 3] });
-//   console.log(getState());
-//   //if the promise is rejected => dispatch()
-// });
-
-// store.dispatch({
-//   type: "error",
-//   payload: { message: "an error occurred." },
-// });
-
-// const unsubscribe = store.subscribe(() => {
-//   console.log("Store changed!", store.getState());
-// });
-
-// store.dispatch(userAdded({ name: "User 1" }));
-// store.dispatch(userAdded({ name: "User 2" }));
-
-// store.dispatch(projectAdded({ name: "Project 1" }));
-
-// store.dispatch(bugAdded({ description: "Bug 1" }));
-// store.dispatch(bugAdded({ description: "Bug 2" }));
-// store.dispatch(bugAdded({ description: "Bug 3" }));
-
-// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-
-// store.dispatch(bugResolved({ id: 1 }));
-
-// const unresolvedBusgs = getUnresolvedBugs(store.getState());
-// const unresolvedBusgs2 = getUnresolvedBugs(store.getState());
-
-// console.log(unresolvedBusgs === unresolvedBusgs2);
-
-// const bugs = getBugsByUser(1)(store.getState());
-// console.log("bugs by id", bugs);
-
-// unsubscribe(); //para cancelar subcripcion a la store
-
-// store.dispatch(bugRemoved({ id: 1 }));
-
-// console.log("state", store.getState());
+store.dispatch(loadBugs());
--- redux-starter/src/store/bugs.js
+++ redux-starter/src/store/bugs.js
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "reselect";
+import { apiCallBegan } from "./api";
 
 let lastId = 0;
 
@@ -11,6 +12,9 @@ const slice = createSlice({
     lastFetch: null,
   },
   reducers: {
+    bugsReceived: (bugs, action) => {
+      bugs.list = action.payload;
+    },
     bugAssignedToUser: (bugs, action) => {
       const { bugId, userId } = action.payload;
       const index = bugs.list.findIndex((bug) => bug.id === bugId);
@@ -36,10 +40,23 @@ const slice = createSlice({
   },
 });
 
-export const { bugAdded, bugRemoved, bugResolved, bugAssignedToUser } =
-  slice.actions;
+export const {
+  bugAdded,
+  bugRemoved,
+  bugResolved,
+  bugAssignedToUser,
+  bugsReceived,
+} = slice.actions;
 
 export default slice.reducer;
+
+//Action Cretor
+const url = "/bugs";
+export const loadBugs = () =>
+  apiCallBegan({
+    url,
+    onSuccess: bugsReceived.type,
+  });
 
 //Selector
 // export const getUnresolvedBugs = (state) =>
