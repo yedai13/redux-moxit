1384f55749aa88158e61e3d9a918062772c7eb60
Facundo Marin <facundo.marin2012@gmail.com>
2021-12-30 11:02:51 -0300

    parameterizing middleware

--- redux-starter/src/store/configureStore.js
+++ redux-starter/src/store/configureStore.js
@@ -3,7 +3,10 @@ import reducer from "./reducer";
 import logger from "./middleware/logger";
 
 export default function () {
-  return configureStore({ reducer, middleware: [logger] });
+  return configureStore({
+    reducer,
+    middleware: [logger({ destination: "console" })],
+  });
 }
 // import { createStore } from "redux";
 // import { devToolsEnhancer } from "redux-devtools-extension";
