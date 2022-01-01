fbdd05017ed0f96ea354559109a7e4423f6c533c
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-01 17:50:18 -0300

    assigning a bug to a user

--- redux-starter/src/store/bugs.js
+++ redux-starter/src/store/bugs.js
@@ -23,7 +23,7 @@ const slice = createSlice({
       bugs.loading = false;
     },
     bugAssignedToUser: (bugs, action) => {
-      const { bugId, userId } = action.payload;
+      const { id: bugId, userId } = action.payload;
       const index = bugs.list.findIndex((bug) => bug.id === bugId);
       bugs.list[index].userId = userId;
     },
@@ -88,6 +88,14 @@ export const resolveBug = (id) =>
     method: "patch",
     data: { resolved: true },
     onSuccess: bugResolved.type,
+  });
+
+export const assignBugToUser = (bugId, userId) =>
+  apiCallBegan({
+    url: url + "/" + bugId,
+    method: "patch",
+    data: { userId },
+    onSuccess: bugAssignedToUser.type,
   });
 
 //Selector
