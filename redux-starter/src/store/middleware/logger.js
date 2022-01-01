3514ff58cf84bcc974ad1547b6f6753684c76f28
Facundo Marin <facundo.marin2012@gmail.com>
2022-01-01 20:01:00 -0300

    social tests-converted

--- redux-starter/src/store/middleware/logger.js
+++ redux-starter/src/store/middleware/logger.js
@@ -1,7 +1,7 @@
 //SNA
 const logger = (param) => (store) => (next) => (action) => {
   console.log("Logging", param);
-  next(action);
+  return next(action);
 };
 
 export default logger;
--- redux-starter/src/store/middleware/toast.js
+++ redux-starter/src/store/middleware/toast.js
@@ -1,6 +1,6 @@
 const toast = (store) => (next) => (action) => {
   if (action.type === "error") console.log("toastify", action.payload.message);
-  else next(action);
+  else return next(action);
 };
 
 export default toast;
